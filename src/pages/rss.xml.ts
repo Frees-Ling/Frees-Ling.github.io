3419d76f65c1a47d41ae366d837cd786522c85a4
Frees-Ling <freesling496@gmail.com>
2024-02-22 08:41:14 +0000

    update safe

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,6 +2,9 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import getSortedPosts from "@utils/getSortedPosts";
 import { SITE } from "@config";
+// import sanitizeHtml from "sanitize-html";
+// import MarkdownIt from "markdown-it";
+// const parser = new MarkdownIt();
 
 export async function GET() {
   const posts = await getCollection("blog");
@@ -10,11 +13,13 @@ export async function GET() {
     title: SITE.title,
     description: SITE.desc,
     site: SITE.website,
+    stylesheet: "/rss/styles.xsl",
     items: sortedPosts.map(({ data, slug }) => ({
-      link: `posts/${slug}/`,
+      link: `posts/${slug}`,
       title: data.title,
       description: data.description,
       pubDate: new Date(data.modDatetime ?? data.pubDatetime),
+      // content: sanitizeHtml(parser.render(data.content)),
     })),
   });
 }
